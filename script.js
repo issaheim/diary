706579410c984b33f51a5e1a6ca943a797ce8aa4
issaheim <75672568+issaheim@users.noreply.github.com>
2021-02-11 20:13:47 +0100

    created function to print diary posts and styled the page

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const header = document.createElement("header");
-const main = document.createElement("main");
-const writeDiv = document.createElement("div");
-const diaryDiv = document.createElement("div");
 
 const diaryForm = document.createElement('form');
 const dateLabel = document.createElement('label');
@@ -10,24 +6,38 @@ const diaryTextLabel = document.createElement('label');
 const diaryText = document.createElement('textarea');
 const saveBtn = document.createElement('input');
 
+
 // -------------------- HEADER --------------------
+const header = document.createElement("header");
 const diaryTitle = document.createElement("h1");
 diaryTitle.innerHTML = "My diary";
 
 const date = new Date();
 
-
-document.body.appendChild(header);
+document.body.insertAdjacentElement("afterbegin", header);
 header.appendChild(diaryTitle);
 
+
+// -------------------- BODY --------------------
+const main = document.createElement("main");
+const writeDiv = document.createElement("div");
+const diaryDiv = document.createElement("div");
+
+writeDiv.id = "writeDiv";
+diaryDiv.id = "diaryDiv";
+
+document.body.insertAdjacentElement("beforeend", main)
+
+
 // ------------------- DIARYFORM -------------------
-document.body.appendChild(writeDiv);
+main.appendChild(writeDiv);
 writeDiv.innerHTML = '';
 
 diaryForm.setAttribute('onSubmit', 'event.preventDefault()');
 
 dateLabel.setAttribute('for', 'inputDate');
 dateLabel.innerText = 'Today´s date:';
+dateLabel.id = "dateLabel";
 
 inputDate.setAttribute('type', 'text');
 inputDate.setAttribute('name', 'inputDate');
@@ -38,12 +48,15 @@ inputDate.required = true;
 diaryTextLabel.setAttribute('for', 'diaryText');
 
 diaryText.setAttribute('type', 'text');
+// diaryText.setAttribute('rows', "10");
+// diaryText.setAttribute('cols', "35");
 diaryText.setAttribute('name', 'diaryText');
 diaryText.setAttribute('id', 'diaryText');
 diaryText.setAttribute('placeholder', 'Text');
 diaryText.required = true;
 
 saveBtn.setAttribute('type', 'submit');
+saveBtn.id = "saveBtn";
 saveBtn.value = 'Save';
 // saveBtn.addEventListener('click', checkCredentials)
 
@@ -55,6 +68,7 @@ diaryForm.appendChild(saveBtn);
 
 writeDiv.appendChild(diaryForm);
 
+
 // ------------------- Array for posts -------------------
 let diaryPosts = []
 
@@ -62,8 +76,9 @@ if (localStorage.getItem("savedDiaryPosts") == null){
   localStorage.setItem("savedDiaryPosts", JSON.stringify(diaryPosts));
   console.log("array finns ej");
 }
-// ---------------------- Save posts ----------------------
 
+
+// ---------------------- Save posts ----------------------
 function savePost(inputDate, diaryText){
   let localDiaryPost = JSON.parse(localStorage.getItem("savedDiaryPosts"))
   let post = {date: inputDate, text: diaryText}
@@ -73,23 +88,36 @@ function savePost(inputDate, diaryText){
   localStorage.setItem("savedDiaryPosts", JSON.stringify(localDiaryPost));
 }
 
+
 // --------------------- saveBtn ---------------------
 saveBtn.addEventListener("click", function(){
   console.log(diaryPosts);
   savePost(inputDate.value, diaryText.value)
+  inputDate.innerHTML = '';
+  diaryText.innerHTML = '';
 
   printDiary();
 });
 
 
 // -------------------- Print diary --------------------
+main.appendChild(diaryDiv);
+let diaryPostList = document.createElement("ul");
+diaryDiv.appendChild(diaryPostList);
+
 function printDiary(){
-  let localDiaryPost = JSON.parse(localStorage.getItem("savedDiaryPosts"))
+  let localDiaryPost = JSON.parse(localStorage.getItem("savedDiaryPosts"));
+  
+  diaryPostList.innerHTML = "";
 
   for (post in localDiaryPost){
     console.log(localDiaryPost[post].date);
     console.log(localDiaryPost[post].text);
+    
+    diaryPostList.insertAdjacentHTML("afterbegin",`<li id="savedDiaryPost"><h2>${localDiaryPost[post].date}</h2> <p>${localDiaryPost[post].text}</p></li>`);
   }
 }
 
 printDiary();
+
+console.log(localDiaryPost[post].date);
